c15460a63b31ecb88bef782ce4d157f32c45c167
Justinus <justinus@live.com>
2015-02-17 13:29:03 -0800

    Added some methods/variables
    Added a few methods and variables into the project

--- ATMProject/ATMProject/ATM.h
+++ ATMProject/ATMProject/ATM.h
@@ -5,6 +5,8 @@ class ATM
 private:
 	int testInt;
 
+
+
 public:
 
 	ATM(void)
--- ATMProject/ATMProject/Customer.h
+++ ATMProject/ATMProject/Customer.h
@@ -1,9 +1,19 @@
 #pragma once
+#include <string>
+#include "Account.h"
 
 class Customer
 {
 private:
+	std::string name;
+	std::string address;
+	std::string phone;
+	Account** accounts;
 
 public:
+	std::string getName(void)
+	{
+		return name;
+	}
 
 };
