b7ae3f40cc486c7a0e8b5cc0acc901dc3e3b22b2
AdamCam <cameronac83@gmial.com>
2015-02-17 16:17:06 -0800

    Added unit tests
    Not sure they are all right

--- ATMUnitTest/ATMUnitTest/unittest1.cpp
+++ ATMUnitTest/ATMUnitTest/unittest1.cpp
@@ -30,37 +30,59 @@ namespace ATMUnitTest
 
 		TEST_METHOD(ATMGetCustomerInfoValid)//Tests GetCustomerInfo in ATM class with a customer object that has valid info
 		{
+			//customer.setName("Adam Cameron");
+			//customer.setAddress("151 Green Ln");
+			//customer.setPhone("777-777-7777");
+			//Assert::AreEqual(customer.getName(), "Adam Cameron");
+			//Assert::AreEqual(customer.getAddress(), "151 Green Ln");
+			//Assert::AreEqual(customer.getPhone(), "777-777-7777");
 
 		}
 
 		TEST_METHOD(ATMGetCustomerInfoInvalid)//Tests GetCustomerInfo in ATM class with a customer object that has no info
 		{
-			
+			//customer.setName("Adam Cameron");
+			//customer.setAddress("151 Green Ln");
+			//customer.setPhone("777-777-7777");
+			//Assert::AreEqual(customer.getName(), "");
+			//Assert::AreEqual(customer.getAddress(), "");
+			//Assert::AreEqual(customer.getPhone(), "");
 		}
 
 		TEST_METHOD(ATMGetBalance)//Tests GetBalance on the ATM class with no account pointer
 		{
-			
+			//atm.setBalance(500);
+			//atm.getBalance();
+			//Assert::AreEqual(500, atm.getBalance);
+
 		}
 
 		TEST_METHOD(ATMWithdrawlValid)//Tests Withdrawal on the ATM class when the ATM has sufficient funds
 		{
-			
+			//atm.withdraw(500);
+			//Assert::AreEqual(500, atm.getBalance());
+
 		}
 
 		TEST_METHOD(ATMWithdrawlInvalid)//Tests Withdrawal on the ATM class when the ATM has insufficient funds
 		{
-			
+			//atm.setBalance(200);
+			//atm.withdrawal(500);
+			//Assert::AreEqual(atm.getBalance(), atm.withdrawal());
+
 		}
 
 		TEST_METHOD(ATMDeposit)//Tests deposit on the ATM class
 		{
-			
+			//customer.deposit(500);
+			//Assert::AreEqual(500, account1->getBalance());
 		}
 
 		TEST_METHOD(ATMSetPin)//Tests SetPin on the ATM class
 		{
-			
+			//atm.setPin(5555)
+			//atm.verifyNewPin(5555)
+			//Assert::AreEqual(setPin, verifyNewPin);
 		}
 
 		//JAMES Section
