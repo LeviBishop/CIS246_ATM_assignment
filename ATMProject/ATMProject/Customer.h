3dd9265ee86d832620bcc0da633a60a580394408
grimscyth <grimscyth@gmail.com>
2015-02-26 11:12:57 -0800

    Customer
    Added in customer class methods.

--- ATMProject/ATMProject/Customer.h
+++ ATMProject/ATMProject/Customer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 #include "Account.h"
 
 /*
@@ -20,12 +21,90 @@ private:
 	std::string name;
 	std::string address;
 	std::string phone;
-	Account** accounts;
+	std::vector <Account*> accounts;
+	int numAccounts;
 
 public:
+	Customer(std::string newName, std::string newAddress, std::string newPhone)
+	{
+		numAccounts = 0;
+		name = newName;
+		address = newAddress;
+		phone = newPhone;
+	}
 	std::string getName(void)
 	{
 		return name;
 	}
+	std::string getAddress(void)
+	{
+		return address;
+	}
+	std::string getPhone(void)
+	{
+		return phone;
+	}
+	void setName(std::string newName)
+	{
+		name = newName;
+	}
 
+	void setAddress(std::string newAddress)
+	{
+		address = newAddress;
+	}
+
+	void setPhone(std::string newPhone)
+	{
+		phone = newPhone;
+	}
+
+	void addAccount(int pin, int accountNumber, this)
+	{
+		accounts[numAccounts] = new Account();
+		numAccounts++;
+	}
+
+	void removeAccount(int accountNumber)
+	{
+		for (int i = 0, i < numAccounts; i++)
+		{
+			if (accountNumber == accounts[numAccounts]->getAccountNum())
+			{
+				delete accounts[i];
+				accounts->erase(i);
+				break;
+			}
+		}
+	}
+
+	std::vector<Account*>* getAccounts()
+	{
+		return accounts[];
+	}
+
+	void transferBalance(double amount, int accountFrom, int accountTo)
+	{
+		int accountIndexFrom;
+		int accountIndexTo;
+
+		accountIndexFrom = getAccountIndex(accountFrom);
+		accountIndexTo = getAccountIndex(accountTo);
+
+		if (accounts[accountIndexFrom]->transferOut(double amount))
+		{
+			accounts[accountIndexTo]->transferIn(double amount);
+		}
+	}
+
+	int getAccountIndex(int accountNum)
+	{
+		for (int i = 0; i < numAccounts; i++)
+		{
+			if (accountNum == accounts[i]->getAccountNum)
+			{
+				return i;
+			}
+		}
+	}
 };
